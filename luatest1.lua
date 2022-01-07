15e4dd44f07baa1f6800410b6e3171cda2912c41
Techrosity <88183670+Techrosity@users.noreply.github.com>
2022-01-07 08:22:49 +0700

    Update luatest1.lua

--- luatest1.lua
+++ luatest1.lua
@@ -14,3 +14,41 @@ if detected
 else
   rick.Playing = false
 end
+
+--GUI TEST CODE
+
+local imageButton = script.Parent
+ 
+local colorNormal = Color3.new(1, 1, 1) -- white
+local colorHover = Color3.new(0, 1, 0) -- green
+local colorPress = Color3.new(1, 0, 0) -- red
+ 
+imageButton.Image = "rbxassetid://787458668"
+imageButton.BackgroundTransparency = 1
+ 
+local function onActivated()
+	print("Let's open the inventory!")
+end
+ 
+local function onPressed()
+	imageButton.ImageColor3 = colorPress
+end
+ 
+local function onReleased()
+	imageButton.ImageColor3 = colorHover
+end
+ 
+local function onEntered()
+	imageButton.ImageColor3 = colorHover
+end
+ 
+local function onLeft()
+	imageButton.ImageColor3 = colorNormal
+end
+ 
+imageButton.MouseEnter:Connect(onEntered)
+imageButton.MouseLeave:Connect(onLeft)
+imageButton.MouseButton1Down:Connect(onPressed)
+imageButton.MouseButton1Up:Connect(onReleased)
+imageButton.Activated:Connect(onActivated)
+onLeft()
